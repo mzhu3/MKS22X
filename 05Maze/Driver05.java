3f5444d5e1d5af92caca796aeab340bccb908a66
Matthew Zhu <mzhu3@stuy.edu>
2016-02-28 21:32:40 -0500

    maze

--- 05Maze/Driver05.java
+++ 05Maze/Driver05.java
@@ -2,12 +2,12 @@ public class Driver05{
 
     public static void main(String[]args){
         Maze f;
-        f = new Maze("data2.dat",true);//true animates the maze.
+        f = new Maze("data1.dat",false);//true animates the maze.
         
         //f.clearTerminal();
 	// f.solve();
-	System.out.println(f.maze[0].length);
-	System.out.println(f.maze.length);
+	System.out.println(f.toString());
+
 	// f.clearTerminal();
 	// System.out.println(f);
     }
