f191b3a635934cbef7e97b9d76ed28b7c7a2e8c6
Matthew Zhu <mzhu3@stuy.edu>
2016-03-29 23:47:54 -0400

    fixed isMatching

---
+++ 09StackQueue/ParenDemo.java
@@ -0,0 +1,55 @@
+public class ParenDemo{
+    public static boolean isMatching(String s){
+	MyStack<String> StringStack = new MyStack<String>();
+	if(s.substring(0,1).equals(")")||
+	   s.substring(0,1).equals("}")||
+	   s.substring(0,1).equals("]")||
+	   s.substring(0,1).equals(">")||
+	   s.substring(s.length()- 1,s.length()).equals("(")||
+	   s.substring(s.length()- 1,s.length()).equals("{")||
+	   s.substring(s.length()- 1,s.length()).equals("[")||
+	   s.substring(s.length()- 1,s.length()).equals("<")){
+	    return false;
+	}
+        for(int i = 0; i < s.length(); i++){
+	    if(s.substring(i,i+1).equals("(")||
+	       s.substring(i,i+1).equals("[")||
+	       s.substring(i,i+1).equals("{")||
+	       s.substring(i,i+1).equals("<")){
+		StringStack.push(s.substring(i,i+1));
+	    }
+	    if(s.substring(i,i+1).equals(")")){
+		if(!StringStack.pop().equals("(")){
+		    return false;
+		}
+	    }
+	    if(s.substring(i,i+1).equals("]")){
+		if(!StringStack.pop().equals("[")){
+		    return false;
+		}
+	    }
+	    if(s.substring(i,i+1).equals("}")){
+		if(!StringStack.pop().equals("{")){
+		    return false;
+		}
+	    }
+	    if(s.substring(i,i+1).equals(">")){
+		if(!StringStack.pop().equals("<")){
+		    return false;
+		}
+	    }
+	}
+	return StringStack.isEmpty();
+    }
+    public static void main(String[]args){
+	String input = "()()(([[]]))";
+	if(args.length > 0){
+	    input = args[0];
+	    System.out.println( isMatching(input)); 
+	}else{
+	    System.out.println("Usage:"); 
+	    System.out.println("java ParenDemo \"text\""); 
+	}
+ 
+    }
+}
