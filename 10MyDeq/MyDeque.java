11653c1cc3e8112371eee36ac9aa5c4725c9fd69
Matthew Zhu <mzhu3@stuy.edu>
2016-04-01 12:31:10 -0400

    start MyDeq

--- 10MyDeq/MyDeque.java
+++ 10MyDeq/MyDeque.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 public class MyDeque<T>{
     private int size;
-    private objects[] data;
+    private Object[] data;
     private int start;
     private int end;
 
     public MyDeque(){
-	data = new objects[4];
+	data = new Object[4];
 	size = 0;
     }
     private int size(){
 	return size;
     }
     private void grow(){
-        objects[]temp = new objects[data.size * 2];
+        Object[]temp = new Object[size() * 2];
 	int counter = 0;
 	for(int i = 0; i < size(); i ++){
-	    if(start != data.size){
+	    if(start != size){
 		temp[i] = data[start];
 		start ++;
 	    }
@@ -26,5 +26,7 @@ public class MyDeque<T>{
 	}
 	data = temp;
     }
+    public void addFirst(T value){
+	
 }
     
