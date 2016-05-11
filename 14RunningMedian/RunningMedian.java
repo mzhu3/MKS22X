61185d26749567bd1fca40c1b6a0ea52f5c5ad40
Matthew Zhu <mzhu3@stuy.edu>
2016-05-11 02:31:50 -0400

    finish Running Median

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -19,14 +19,67 @@ public class RunningMedian{
     public void add(Integer x){
 	if(minHeap.getSize() == 0 && maxHeap.getSize() == 0){
 	    median =(double) x;
+	    minHeap.add(x);
 	}
 	else{
 	    if((double)x < median){
-		minHeap.add(x);
+		maxHeap.add(x);
+		balance();
 	    }
 	    else{
-		maxHeap.add(x);
+		minHeap.add(x);
+		balance();
 	    }
 	}
-	if(minHeap.getSize()-MaxHeap.getSize()>1){
+    }
+    public void balance(){
+	//	System.out.println(minHeap.getSize());
+	//	System.out.println(maxHeap.getSize());
+	if(minHeap.getSize()-maxHeap.getSize()>1 || maxHeap.getSize()-minHeap.getSize() > 1){
+	    if(minHeap.getSize()>maxHeap.getSize()){
+		int dif =( minHeap.getSize() - maxHeap.getSize())/2;
+		//	System.out.println(dif);
+		while(dif >= 1){
+		    Integer minValue = minHeap.delete();
+		    maxHeap.add(minValue);
+		    dif--;
+		}
+	    }
+	    else{
+		int dif =( maxHeap.getSize()-minHeap.getSize())/2;
+		//	System.out.println(dif);
+		while(dif >= 1){
+		    Integer maxValue = maxHeap.delete();
+		    minHeap.add(maxValue);
+		    dif--;
+		}
+	    }
+	}
+	//	System.out.println(minHeap.getSize());
+	//	System.out.println(maxHeap.getSize());
+        int totalNums = maxHeap.getSize() + minHeap.getSize();
+	if(totalNums %2 == 0){
+	    median =(maxHeap.peek()+minHeap.peek())/2.0;
+	}
+	else{
+	    median = (double)maxHeap.peek();
+	}
+    }
+
+    public static void main(String[] args){
+	RunningMedian rm = new RunningMedian();
+	rm.add(100);
+	System.out.println(rm.getMedian());
+	rm.add(12);
+	rm.add(9);
+	rm.add(200);
+	rm.add(50);
+	System.out.println(rm.getMedian());
+	rm.add(33);
+	rm.add(66);
+	rm.add(3);
+	System.out.println(rm.getMedian());
+    }
+}
+					 
 	    
